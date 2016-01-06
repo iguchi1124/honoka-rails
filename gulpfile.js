f26eaacb6fd71af6df482c3ca708b86a029ca974
Shota Iguchi <e.iguchi1124@gmail.com>
2016-01-06 09:08:55 +0900

    Add build tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,16 @@
 var gulp = require('gulp');
 var clean = require('gulp-clean');
+
+gulp.task('clean', function () {
+  gulp.src('app/assets').pipe(clean({ force: true }));
+});
+
+gulp.task('copy', function() {
+  gulp.src(
+    ['bower_components/bootstrap-sass/assets/**/*']
+  ).pipe(gulp.dest('app/assets'));
+
+  gulp.src(
+    ['bower_components/Honoka/scss/honoka/*']
+  ).pipe(gulp.dest('app/assets/stylesheets/honoka'));
+});

a99a46916a9d43ed2eb2c4a3b0f5d4cc0b8eaeff
Shota Iguchi <e.iguchi1124@gmail.com>
2016-01-06 09:40:54 +0900

    Fix build tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var clean = require('gulp-clean');
+var rename = require("gulp-rename");
 
 gulp.task('clean', function () {
   gulp.src('assets').pipe(clean({ force: true }));
@@ -13,4 +14,8 @@ gulp.task('copy', function() {
   gulp.src(
     ['bower_components/Honoka/scss/honoka/*']
   ).pipe(gulp.dest('assets/stylesheets/honoka'));
+
+  gulp.src('bower_components/Honoka/scss/bootstrap.scss')
+    .pipe(rename('_honoka.scss'))
+    .pipe(gulp.dest('assets/stylesheets/'));
 });

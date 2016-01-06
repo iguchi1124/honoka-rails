acc2bb1b757f93d8dbab85388304085f07e746c7
Shota Iguchi <e.iguchi1124@gmail.com>
2016-01-06 10:10:54 +0900

    Add precompile paths for sprockets

--- lib/honoka_rails/engine.rb
+++ lib/honoka_rails/engine.rb
@@ -1,5 +1,15 @@
 module HonokaRails
   class Engine < ::Rails::Engine
-    isolate_namespace HonokaRails
+    initializer 'honoka-rails.assets.precompile' do |app|
+      %w(stylesheets javascripts fonts images).each do |sub|
+        app.config.assets.paths << root.join('assets', sub).to_s
+      end
+
+      # sprockets-rails 3 tracks down the calls to `font_path` and `image_path`
+      # and automatically precompiles the referenced assets.
+      unless Sprockets::Rails::VERSION.split('.', 2)[0].to_i >= 3
+        app.config.assets.precompile << %r(bootstrap/glyphicons-halflings-regular\.(?:eot|svg|ttf|woff2?)$)
+      end
+    end
   end
 end
